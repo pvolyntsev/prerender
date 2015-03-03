8ef481a076f903d98a8895fa67e66d10ea5be15c
Pavel Volyntsev <pavel@visualpharm.com>
2015-03-03 17:28:22 +0600

    flexible options

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+var options = {
+    cache: process.argv.cache || process.env.PHANTOM_CACHE,
+    workers: process.argv.workers || process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
+    phantomBasePort: process.argv.port || process.env.PHANTOM_CLUSTER_BASE_PORT || 3000,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-});
+}
 
+var server = prerender(options);
 
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+if (process.argv.debug||false)
+    server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
+server.use(prerender.removeNgAttrs());
+server.use(prerender.compressHtml());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
-
+switch(options.cache)
+{
+    case 'memory':
+        server.use(prerender.inMemoryHtmlCache());
+        break;
+    case 's3':
+        server.use(prerender.s3HtmlCache());
+        break;
+    case 'file':
+        server.use(require('prerender-file-cache'));
+}
 server.start();
